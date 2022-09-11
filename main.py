80ab69dadddb6638279282714f143a4ef4dcd7e8
Bom <11377ny@gmail.com>
2022-09-10 22:26:43 -0400

    Add export function

--- main.py
+++ main.py
@@ -13,11 +13,14 @@
 
 # save_to_file(keyword, jobs)
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, send_file
 from extractors.rok import extract_rok_jobs
 from extractors.wwr import extract_wwr_jobs
+from file import save_to_file
 
 app = Flask("Scrapper")
+
+db = {}
 
 
 @app.route("/")
@@ -28,10 +31,27 @@ def home():
 @app.route("/jobs")
 def jobs():
     keyword = request.args.get("keyword")
-    rok = extract_rok_jobs(keyword)
-    wwr = extract_wwr_jobs(keyword)
-    jobs = rok + wwr
+    if keyword == None:
+        return redirect("/")
+    if keyword in db:
+        jobs = db[keyword]
+    else:
+        rok = extract_rok_jobs(keyword)
+        wwr = extract_wwr_jobs(keyword)
+        jobs = rok + wwr
+        db[keyword] = jobs
     return render_template("jobs.html", keyword=keyword, jobs=jobs)
+
+
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
+    if keyword not in db:
+        return redirect(f"/search?keyword={keyword}")
+    save_to_file(keyword, db[keyword])
+    return send_file(f"{keyword}.csv", as_attachment=True)
 
 
 app.run("0.0.0.0")
