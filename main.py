021bbfa60d0bcdc0740a264e92b38861a304e18e
Bom <11377ny@gmail.com>
2022-09-10 21:21:18 -0400

    Make HTML files using Flask

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
-from requests import get
-from bs4 import BeautifulSoup
-from extractors.rok import extract_rok_jobs
-from extractors.wwr import extract_wwr_jobs
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+# from requests import get
+# from bs4 import BeautifulSoup
+# from extractors.rok import extract_rok_jobs
+# from extractors.wwr import extract_wwr_jobs
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+# from file import save_to_file
 
-keyword = input("What do you want to search for?")
+# keyword = input("What do you want to search for?")
 
-file = open(f"{keyword}.csv", 'w')
-file.write("Position,Company,Location\n")
+# rok = extract_rok_jobs(keyword)
+# wwr = extract_wwr_jobs(keyword)
 
-rok = extract_rok_jobs(keyword)
-wwr = extract_wwr_jobs(keyword)
+# jobs = rok + wwr
 
-jobs = rok + wwr
+# save_to_file(keyword, jobs)
 
-for job in jobs:
-    file.write(
-        f"{job['position']}, {job['company']}, {job['location']}\n"
-    )
+from flask import Flask, render_template
 
-file.close()
+app = Flask("Scrapper")
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/jobs")
+def jobs():
+    return render_template("jobs.html")
+
+
+app.run("0.0.0.0")
